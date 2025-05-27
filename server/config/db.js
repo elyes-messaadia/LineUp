4aa468f48cf68b34b54ede2fc30d1083709caacd
Elyes MESSAADIA <elyes.messaadia@laplateforme.io>
2025-05-27 03:31:19 +0200

    fix: refactorisation de la fonction de connexion à MongoDB pour simplifier la syntaxe et améliorer la gestion des erreurs concernant MONGO_URI

--- server/config/db.js
+++ server/config/db.js
@@ -1,17 +1,16 @@
+// config/db.js
 const mongoose = require("mongoose");
 
-const connectDB = async () => {
+async function connectDB() {
   try {
     const uri = process.env.MONGO_URI;
-    if (!uri) {
-      throw new Error("⚠️ MONGO_URI est vide !");
-    }
+    if (!uri) throw new Error("❌ MONGO_URI non défini");
 
     await mongoose.connect(uri);
     console.log("✅ Connexion MongoDB réussie");
   } catch (err) {
     console.error("❌ Erreur MongoDB :", err.message);
   }
-};
+}
 
 module.exports = connectDB;
