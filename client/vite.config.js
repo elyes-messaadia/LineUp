dc2ba4d5af1e6cbde9275076933a16fe333dee30
Elyes MESSAADIA <168727602+elyes-messaadia@users.noreply.github.com>
2025-06-01 13:35:25 +0200

    feat: mise à jour des dépendances dans package.json pour améliorer la compatibilité et les performances, refactorisation de la configuration Vite pour activer les sourcemaps en développement et ajustement de la minification avec esbuild, tout en optimisant la gestion des chunks pour les bibliothèques externes

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,10 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      // Désactiver le fast refresh en production
-      fastRefresh: process.env.NODE_ENV !== 'production',
-    }),
+    react(),
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['assets/icon.png'],
@@ -36,26 +33,22 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: false, // Désactiver les sourcemaps en production
-    minify: 'terser', // Utiliser terser pour la minification
-    terserOptions: {
-      compress: {
-        drop_console: true, // Supprimer les console.log en production
-        drop_debugger: true
-      }
-    },
+    sourcemap: process.env.MODE === 'development',
+    minify: 'esbuild',
     rollupOptions: {
       output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom'],
-          'router': ['react-router-dom'],
-          'utils': ['@heroicons/react', 'classnames']
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) return 'vendor-react';
+            if (id.includes('framer-motion')) return 'vendor-framer';
+            return 'vendor';
+          }
         }
       }
     }
   },
   optimizeDeps: {
-    include: ['react', 'react-dom', 'react-router-dom']
+    include: ['react', 'react-dom', 'react-router-dom', 'classnames', 'framer-motion']
   },
   server: {
     port: 5173,
