ee0393d9bb75dfa56dee856e3cf1dd331ac73311
Elyes MESSAADIA <elyes.messaadia@laplateforme.io>
2025-09-14 21:59:18 +0200

    fix(Home): correction du chemin d'accès à l'image des étapes du processus dans le composant Home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -303,7 +303,7 @@ export default function Home() {
           <div className="max-w-5xl mx-auto p-4">
             <div className="overflow-x-auto pb-2 shadow-sm rounded-lg bg-gray-50/50 border border-gray-100">
               <img
-                src="/src/assets/process-steps.svg"
+                src="../assets/process-steps.svg"
                 alt="Étapes du processus"
                 className="w-full min-w-[800px] p-4"
               />
